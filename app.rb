d3f59a30b7b10bd577afd79a747fc2e4114c41c7
github username <github email address>
2019-01-28 17:03:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+
+  text_from_user = params[:user_text]
+
+  @pigged_text = PigLatinizer.new(text_from_user)
+
+  erb :results
+end
+
+
 end

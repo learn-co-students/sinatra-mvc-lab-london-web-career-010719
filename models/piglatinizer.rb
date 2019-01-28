d5c02fa3a56897d456ee86b38b17f0320f888cea
kkoomin <minha.koo.9@gmail.com>
2019-01-28 18:04:34 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,17 +9,17 @@ class PigLatinizer
     def piglatinize(text)
         words = text.split(" ")
         words.map do |w|
-            if w == "every"
-                w = "ereyevay"   
-            elsif w == "another"
-                w = "otheranay"
-            elsif w == "under"
-                w = "erunday"
-            elsif w == "island"
-                w = "andislay"
-            elsif w == "elegant"
-                w = "egantelay"
-            elsif w[0].match(/[aAeEiIoOuU]/)
+            # if w == "every"
+            #     w = "ereyevay"   
+            # elsif w == "another"
+            #     w = "otheranay"
+            # elsif w == "under"
+            #     w = "erunday"
+            # elsif w == "island"
+            #     w = "andislay"
+            # elsif w == "elegant"
+            #     w = "egantelay"
+            if w[0].match(/[aAeEiIoOuU]/)
                 w = w + "way"
             elsif !w[0].match(/[aAeEiIoOuU]/) && !w[1].match(/[aAeEiIoOuU]/) && !w[2].match(/[aAeEiIoOuU]/)
                 w = w[3..-1] + w[0..2] + "ay"
